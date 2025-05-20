50f848c20d97c508233adfd8f492ce635792e19b
linjishen.dev <linjishen.dev@bytedance.com>
2025-05-20 16:38:22 +0800

    feat: upload

--- src/layouts/history/index.js
+++ src/layouts/history/index.js
@@ -1,35 +1,13 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// @mui material components
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
-
-// Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-
-// Material Dashboard 2 React example components
 import DashboardLayout from "components/DashboardLayout";
 import DashboardNavbar from "components/DashboardNavbar";
 import DataTable from "components/DataTable";
-
-// Data
 import authorsTableData from "./data/authorsTableData";
 
-function Tables() {
+function History() {
   const { columns, rows } = authorsTableData();
 
   return (
@@ -70,4 +48,4 @@ function Tables() {
   );
 }
 
-export default Tables;
+export default History;
